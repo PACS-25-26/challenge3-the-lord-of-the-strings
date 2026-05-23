10731c4a91fd21626618172390a08553393ac335
David Laurenti <david@linux.lauro>
2026-05-23 19:54:25 +0200

    First packages settings

---
+++ include/Alias.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file Alias.hpp
+ * @author Elena Vera Cassano and David Laurenti
+ * @brief Aliases file containing all common type aliases used.
+ * @version 1.0
+ * @date 23-05-2026
+ * @details 
+ */
+
+ #pragma once
+
+ #include <iostream>
+ #include <vector>
+ #include <string>
+ #include <cmath>
+ #include <memory>
+
+
+ #include <mpi.h>
+ #include <omp.h>
+
+ #include <Eigen/Dense>
+ #include <muParser.h>
+ 
