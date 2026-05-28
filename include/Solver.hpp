272b3bd110ad8a4ea9eff4164916c579dd57a13c
David Laurenti <david.laurenti@mail.polimi.it>
2026-05-28 17:32:33 +0200

    Added second constructor, private function to set data and apply dirichlet boundary conditions

--- include/Solver.hpp
+++ include/Solver.hpp
@@ -20,24 +20,81 @@
     * @class Solver
     * @brief Class implementing the Jacobi algorithm to solve the Laplace equation.
     * @details The solver is the most important feature of the project. 
-    * @invariant
-    * @note 
     */
     class Solver {
         private:
+            /**
+             * @brief Matrix representing local processor solution at each iteration.
+             * @details The matrix U is updated iteratively in the solve() method. After convergence, it contains the final solution of the Laplace equation for the local portion of the domain handled by the processor.
+             */
             Matrix U;
-            Matrix F; // Capire se metterla solo in solve()
 
-            SolverConfig s_config; // Capire se fare struct o dati singoli
+            /**
+             * @brief Matrix representing the source term of the Laplace equation.
+             * @details The matrix F is initialized based on the source function provided in the SolverConfig. It is used in the iterative update of the solution matrix U in the solve() method.
+             */
+            Matrix F;
+
+            /**
+             * @brief Configuration parameters for the solver of the Laplace equation.
+             * @details This struct contains all the necessary parameters to define the problem to be solved, such as the exact solution, the source term, the Dirichlet boundary conditions, the number of grid points, the grid spacing, the tolerance for convergence, and the maximum number of iterations.
+             */
+            SolverConfig s_config;
+
+            /**
+             * @brief Configuration parameters for parallel execution.
+             * @details This struct contains all the necessary parameters for parallelization, such as rank, size, local number of rows and columns, ranks of neighboring processes.
+             */
             ParallelConfig p_config;
+            
+            /**
+             * @brief Initializes the solver based on the provided configurations.
+             * @details This function sets up initial conditions, and is called by both constructors to ensure that the solver is properly initialized regardless of how it is constructed.
+             */ 
+            void setup();
 
+            /**
+             * @brief Checks for convergence of the Jacobi method.
+             * @param U_old The solution matrix from the previous iteration.
+             * @param U_new The solution matrix from the current iteration.
+             * @return True if the method has converged, false otherwise.
+             * @details The convergence is checked by computing the mean squared error between U_old and U_new.
+             */
             bool check_convergence(const Matrix& U_old, const Matrix& U_new);
+            
+            /**
+             * @brief Applies the Dirichlet boundary conditions to the solution matrix U.
+             * @details This function modifies the matrix U in-place to enforce the Dirichlet boundary conditions.
+             */
+            void apply_dirichlet_conditions();
 
         public:
+            /** 
+             * @brief Constructor for the Solver class.
+             * @param s Configuration for the solver.
+             * @param p Configuration for parallel execution.
+             * @details The constructor initializes the matrix U and source F based on given configurations.
+             */
             Solver(const SolverConfig& s, const ParallelConfig& p);
+            
+            /**
+             * @brief Constructor for the Solver class.
+             * @param config_file The name of the configuration file containing the solver and parallel parameters.
+             * @details This constructor reads the configuration from a file and initializes the solver accordingly.
+             */
+            explicit Solver(const std::string& config_file);
 
+            /**
+             * @brief Solves the Laplace equation using the Jacobi method.
+             * @details The method firstly applies the Dirichlet boundary conditions, then iteratively updates the solution matrix U until convergence or until the maximum iterations are reached.
+             */
             void solve();
 
+            /**
+             * @brief Converts the solution matrix U to VTK format and writes it to a file.
+             * @param file_name The name of the output VTK file.
+             * @details This function gathers the solution from all processes and writes it in a format suitable for visualize in Paraview.
+             */
             void convert_to_vtk(const std::string& file_name);
     };
  }
--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,153 +1,177 @@
 #include "Solver.hpp"
 
-bool Laplace::Solver::check_convergence(const Matrix& U_old, const Matrix& U_new){
-    Index rows = p_config.loc_rows;
-    Index cols = p_config.loc_cols;
-    Real h = s_config.h;
+namespace Laplace{
+    bool Solver::check_convergence(const Matrix& U_old, const Matrix& U_new){
+        Index rows = p_config.loc_rows;
+        Index cols = p_config.loc_cols;
+        Real h = s_config.h;
 
-    Real local_mse = 0.0;
+        Real local_mse = 0.0;
 
-    #pragma omp parallel for collapse(2) reduction(+:local_mse)
-    for(Index i = 0; i < rows; ++i){
-        for(Index j = 0; j < cols; ++j){
-            Real diff = U_new(i, j) - U_old(i, j);
-            local_mse += diff * diff;
+        #pragma omp parallel for collapse(2) reduction(+:local_mse)
+        for(Index i = 0; i < rows; ++i){
+            for(Index j = 0; j < cols; ++j){
+                Real diff = U_new(i, j) - U_old(i, j);
+                local_mse += diff * diff;
+            }
         }
+
+        local_mse = std::sqrt(h*local_mse);
+
+        int local_convergence = (local_mse < s_config.tol) ? 1 : 0;
+
+        int global_convergence(0);
+
+        MPI_Allreduce(&local_convergence, &global_convergence, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
+
+        return global_convergence == 1;
     }
 
-    local_mse = std::sqrt(h*local_mse);
+    void Solver::solve(){
+        apply_dirichlet_conditions();
 
-    int local_convergence = (local_mse < s_config.tol) ? 1 : 0;
+        Index rows = p_config.loc_rows;
+        Index cols = p_config.loc_cols;
+        Real h = s_config.h;
+        size_t max_it = s_config.max_it;
 
-    int global_convergence(0);
+        // Vectors to hold ghost rows for communication
+        RealVector ghost_row_up(cols, 0.0);
+        RealVector ghost_row_down(cols, 0.0);
 
-    MPI_Allreduce(&local_convergence, &global_convergence, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
+        Matrix U_old = U;
 
-    return global_convergence == 1;
-}
+        size_t it = 0;
+        bool converged = false;
 
-void Laplace::Solver::solve(){
-    Index rows = p_config.loc_rows;
-    Index cols = p_config.loc_cols;
-    Real h = s_config.h;
-    size_t max_it = s_config.max_it;
+        while(!converged && it < max_it){
+            // Exchaning ghost rows with neighbours (handle also the case MPI_PROC_NULL)
+            MPI_Sendrecv(&U_old(0, 0), cols, MPI_DOUBLE, p_config.rank_up, 0,
+                        ghost_row_down.data(), cols, MPI_DOUBLE, p_config.rank_down, 0,
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Sendrecv(&U_old(rows-1, 0), cols, MPI_DOUBLE, p_config.rank_down, 0,
+                        ghost_row_up.data(), cols, MPI_DOUBLE, p_config.rank_up, 0,
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            // Update U using the Jacobi method
+            #pragma omp parallel for collapse(2)
+            for(Index i = 1; i < rows - 1; ++i){
+                for(Index j = 1; j < cols - 1; ++j){
+                    U(i, j) = 0.25 * (U_old(i - 1, j) + U_old(i + 1, j) + U_old(i, j - 1) + U_old(i, j + 1) - h * h * F(i, j));
+                }
+            }
+            
+            // Update up ghost row
+            if(p_config.rank_up != MPI_PROC_NULL){
+                #pragma omp parallel for
+                for(Index j = 1; j < cols-1; ++j){
+                    U(0, j) = 0.25 * (ghost_row_up[j] + U_old(1, j) + U_old(0, j-1) + U_old(0, j+1) - h*h * F(0, j));
+                }
+            }
 
-    // Vectors to hold ghost rows for communication
-    RealVector ghost_row_up(cols, 0.0);
-    RealVector ghost_row_down(cols, 0.0);
+            // Update down ghost row
+            if(p_config.rank_down != MPI_PROC_NULL){
+                #pragma omp parallel for
+                for(Index j = 1; j < cols-1; ++j){
+                    U(rows-1, j) = 0.25 * (U_old(rows-2, j) + ghost_row_down[j] + U_old(rows-1, j-1) + U_old(rows-1, j+1) - h*h * F(rows-1, j));
+                }
+            }
 
-    Matrix U_old = U;
+            converged = check_convergence(U_old, U);
+            U_old = U;
+            ++it;
+        }
 
-    size_t it = 0;
-    bool converged = false;
+        if(p_config.rank == 0){
+            if(converged){
+                std::cout << "Converged in " << it << " iterations." << std::endl;
+            }
+            else{
+                std::cout << "Did not converge after " << max_it << " iterations." << std::endl;
+            }
+        }
 
-    while(!converged && it < max_it){
-        // Exchaning ghost rows with neighbours (handle also the case MPI_PROC_NULL)
-        MPI_Sendrecv(&U_old(0, 0), cols, MPI_DOUBLE, p_config.rank_up, 0,
-                     ghost_row_down.data(), cols, MPI_DOUBLE, p_config.rank_down, 0,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Sendrecv(&U_old(rows-1, 0), cols, MPI_DOUBLE, p_config.rank_down, 0,
-                     ghost_row_up.data(), cols, MPI_DOUBLE, p_config.rank_up, 0,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    // Possibile aggiornamento: aggiungi parametro local_count in p_config e usalo per evitare di fare 
+    void Solver::convert_to_vtk(const std::string& file_name){
+        Index cols = p_config.loc_cols; ///< Global number of columns (same for all processes)
+
+        // Gather global matrix on root process
+        Matrix U_global;
+        if(p_config.rank == 0){
+            U_global.resize(s_config.N, s_config.N);
+        }
         
-        // Update U using the Jacobi method
-        #pragma omp parallel for collapse(2)
-        for(Index i = 1; i < rows - 1; ++i){
-            for(Index j = 1; j < cols - 1; ++j){
-                U(i, j) = 0.25 * (U_old(i - 1, j) + U_old(i + 1, j) + U_old(i, j - 1) + U_old(i, j + 1) - h * h * F(i, j));
-            }
+        // Gather the local sizes from all processes to compute the displacements for MPI_Gatherv
+        std::vector<int> recv_counts;
+        std::vector<int> displs;
+        if(p_config.rank == 0){
+            recv_counts.resize(p_config.size);
+            displs.resize(p_config.size);
         }
         
-        // Update up ghost row
-        if(p_config.rank_up != MPI_PROC_NULL){
-            #pragma omp parallel for
-            for(Index j = 1; j < cols-1; ++j){
-                U(0, j) = 0.25 * (ghost_row_up[j] + U_old(1, j) + U_old(0, j-1) + U_old(0, j+1) - h*h * F(0, j));
+        // Gather the local sizes from all processes
+        int local_size = static_cast<int>(p_config.loc_rows * cols);
+        MPI_Gather(&local_size, 1, MPI_INT, recv_counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        // Compute displacements for MPI_Gatherv
+        if(p_config.rank == 0){
+            displs[0] = 0;
+            for(int i = 1; i < p_config.size; ++i){
+                displs[i] = displs[i-1] + recv_counts[i-1];
             }
         }
 
-        // Update down ghost row
-        if(p_config.rank_down != MPI_PROC_NULL){
-            #pragma omp parallel for
-            for(Index j = 1; j < cols-1; ++j){
-                U(rows-1, j) = 0.25 * (U_old(rows-2, j) + ghost_row_down[j] + U_old(rows-1, j-1) + U_old(rows-1, j+1) - h*h * F(rows-1, j));
+        // Gather the local matrices into the global matrix on the root process
+        MPI_Gatherv(U.data(), local_size, MPI_DOUBLE, U_global.data(), recv_counts.data(), displs.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        // Root process writes the global matrix to a VTK file
+        if(p_config.rank == 0){
+            std::ofstream vtk_file(file_name);
+            if(!vtk_file.is_open()){
+                throw std::runtime_error("Could not open file " + file_name + " for writing .vtk!");
             }
-        }
 
-        converged = check_convergence(U_old, U);
-        U_old = U;
-        ++it;
+            vtk_file << "# vtk DataFile Version 3.0\n";
+            vtk_file << "Laplace solution\n";
+            vtk_file << "ASCII\n";
+            vtk_file << "DATASET STRUCTURED_POINTS\n";
+            vtk_file << "DIMENSIONS " << s_config.N << " " << s_config.N << " 1\n";
+            vtk_file << "ORIGIN 0 0 0\n";
+            vtk_file << "SPACING " << s_config.h << " " << s_config.h << " 1\n";
+            vtk_file << "POINT_DATA " << s_config.N * s_config.N << "\n";
+            vtk_file << "SCALARS U double 1\n";
+            vtk_file << "LOOKUP_TABLE default\n";
+
+            for(Index r = 0; r < s_config.N; ++r){
+                Index i = s_config.N - 1 - r; // Reverse row order for VTK
+                for(Index j = 0; j < s_config.N; ++j){
+                    vtk_file << U_global(i, j) << "\n";
+                }
+            }
+
+            vtk_file.close();
+        }
     }
 
-    if(p_config.rank == 0){
-        if(converged){
-            std::cout << "Converged in " << it << " iterations." << std::endl;
-        }
-        else{
-            std::cout << "Did not converge after " << max_it << " iterations." << std::endl;
-        }
+    Solver::Solver(const std::string& config_file){
+        s_config = read_data(config_file);
+        p_config; //  = Mettere funzione per inizializzare
+        
+        setup(); ///< Initialize the solver based on the provided configurations
     }
 
-}
+    Solver::Solver(const SolverConfig& s, const ParallelConfig& p) : s_config(s), p_config(p){
+        setup();/// < Initialize the solver based on the provided configurations
+    }
 
-// Possibile aggiornamento: aggiungi parametro local_count in p_config e usalo per evitare di fare 
-void Laplace::Solver::convert_to_vtk(const std::string& file_name){
-    Index cols = p_config.loc_cols; ///< Global number of columns (same for all processes)
+    void Solver::setup(){
+        ;
+    }
 
-    // Gather global matrix on root process
-    Matrix U_global;
-    if(p_config.rank == 0){
-        U_global.resize(s_config.N, s_config.N);
+    void Solver::apply_dirichlet_conditions(){
+        ;
     }
     
-    // Gather the local sizes from all processes to compute the displacements for MPI_Gatherv
-    std::vector<int> recv_counts;
-    std::vector<int> displs;
-    if(p_config.rank == 0){
-        recv_counts.resize(p_config.size);
-        displs.resize(p_config.size);
-    }
-    
-    // Gather the local sizes from all processes
-    int local_size = static_cast<int>(p_config.loc_rows * cols);
-    MPI_Gather(&local_size, 1, MPI_INT, recv_counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Compute displacements for MPI_Gatherv
-    if(p_config.rank == 0){
-        displs[0] = 0;
-        for(int i = 1; i < p_config.size; ++i){
-            displs[i] = displs[i-1] + recv_counts[i-1];
-        }
-    }
-
-    // Gather the local matrices into the global matrix on the root process
-    MPI_Gatherv(U.data(), local_size, MPI_DOUBLE, U_global.data(), recv_counts.data(), displs.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    // Root process writes the global matrix to a VTK file
-    if(p_config.rank == 0){
-        std::ofstream vtk_file(file_name);
-        if(!vtk_file.is_open()){
-            throw std::runtime_error("Could not open file " + file_name + " for writing .vtk!");
-        }
-
-        vtk_file << "# vtk DataFile Version 3.0\n";
-        vtk_file << "Laplace solution\n";
-        vtk_file << "ASCII\n";
-        vtk_file << "DATASET STRUCTURED_POINTS\n";
-        vtk_file << "DIMENSIONS " << s_config.N << " " << s_config.N << " 1\n";
-        vtk_file << "ORIGIN 0 0 0\n";
-        vtk_file << "SPACING " << s_config.h << " " << s_config.h << " 1\n";
-        vtk_file << "POINT_DATA " << s_config.N * s_config.N << "\n";
-        vtk_file << "SCALARS U double 1\n";
-        vtk_file << "LOOKUP_TABLE default\n";
-
-        for(Index r = 0; r < s_config.N; ++r){
-            Index i = s_config.N - 1 - r; // Reverse row order for VTK
-            for(Index j = 0; j < s_config.N; ++j){
-                vtk_file << U_global(i, j) << "\n";
-            }
-        }
-
-        vtk_file.close();
-    }
 }
